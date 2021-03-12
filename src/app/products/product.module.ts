1f217bafd315f4c038c405de4944f825dbdca9d4
JR Strayhorn <jrstrayhorn@gmail.com>
2021-03-11 19:31:47 -0500

    registering an effect

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -10,6 +10,8 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 /* NgRx */
 import { StoreModule } from '@ngrx/store';
 import { productReducer } from './state/product.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { ProductEffects } from './state/product.effects';
 
 const productRoutes: Routes = [{ path: '', component: ProductShellComponent }];
 
@@ -18,6 +20,7 @@ const productRoutes: Routes = [{ path: '', component: ProductShellComponent }];
     SharedModule,
     RouterModule.forChild(productRoutes),
     StoreModule.forFeature('products', productReducer),
+    EffectsModule.forFeature([ProductEffects]),
   ],
   declarations: [
     ProductShellComponent,
